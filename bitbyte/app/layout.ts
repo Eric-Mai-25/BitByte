cb9d710cfd489332bfa05da00bbe99f3f7f5ac8f
Eric-Mai-25 <ericmai2@gmail.com>
2025-03-11 14:19:24 -0700

    Home page Sign in

--- bitbyte/app/layout.ts
+++ bitbyte/app/layout.ts
@@ -1,7 +1,22 @@
 import { SessionProvider } from "next-auth/react";
 import type { Metadata } from "next";
-import ".globals.css";
+import "./globals.css";
 
 export const metadata: Metadata = {
-    
+    title: "BitByte",
+    description: 'A simplified review page'
+}
+
+export default function RootLayout({
+    children,
+} : {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="en">
+            <body>
+                <SessionProvider> {children} </SessionProvider>
+            </body>
+        </html>
+    );
 }
