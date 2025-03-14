a5551562ad53928f02f6621d0b78843ce56978fb
Eric-Mai-25 <ericmai2@gmail.com>
2025-03-14 12:35:01 -0700

    Supabase database integration

--- bitbyte/app/(root)/page.tsx
+++ bitbyte/app/(root)/page.tsx
@@ -1,23 +1,11 @@
 "use client";
 import Image from "next/image";
-import { signIn, signOut, useSession } from "next-auth/react"
+import { useSession } from "next-auth/react"
 
 export default function Home() {
   const { data: session } = useSession();
-  if (session) {
-    return (
-      <div>
-        <p>Welcome, {session.user?.name}!</p>
-        <button onClick={() => signOut()}> Sign Out</button>
-      </div>
-    )
-  }
-
-
-  return (
-    <div>
-      <p>You are not signed in.</p>
-      <button onClick={() => signIn('google')}>Sign in with Google</button>
-    </div>
+  return(
+    <>
+    </>
   )
 }
