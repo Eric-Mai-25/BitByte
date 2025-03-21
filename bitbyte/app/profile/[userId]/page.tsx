249491a4c173eaf47e89992fd52729252071c9ee
ericmai2@gmail.com <ericmai2@gmail.com>
2025-03-21 16:56:55 -0700

    fetching user and reviews

--- bitbyte/app/profile/[userId]/page.tsx
+++ bitbyte/app/profile/[userId]/page.tsx
@@ -3,3 +3,17 @@ import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
 import Link from "next/link";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+const prisma = new PrismaClient();
+
+async function getUserAndReviews(userId:string) {
+    const user = await prisma.users.findUnique({
+        where: {id: userId}
+    });
+
+    const reviews = await prisma.review.findMany({
+        where: {userId},
+        orderBy: { createdAt: "desc"}
+    });
+    return {user, reviews};
+}
